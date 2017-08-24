5b5ccf9dd4bf8c14dcaad6c7fef2fe7290afd53e
dev@cruzata <dev.cruzata@gmail.com>
2017-08-24 18:17:43 +0530

    u

---
+++ ERP/Project.Web/Controllers/demo/demoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Web.Controllers.demo
+{
+    public class demoController : Controller
+    {
+        //
+        // GET: /demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
